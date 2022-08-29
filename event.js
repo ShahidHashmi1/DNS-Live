796da21f2401756f58e227d8d72f6d53da108a38
ShahidHashmi1 <hashmi.pgh@gmail.com>
2022-08-29 19:57:14 -0400

    update to event js

--- event.js
+++ event.js
@@ -27,47 +27,34 @@ function setEvent(data, i) {
     eventCard.setAttribute('class', 'text-center')
     eventCard.setAttribute('class', 'container')
     eventCard.setAttribute('id', i)
-    cardRow = document.createElement("div")
-    cardRow.setAttribute('class', 'row')
-    cardCol = document.createElement("div")
-    cardCol.setAttribute('class', 'col-sm-4')
     resultsContainer.append(eventCard)
-    eventCard.append(cardRow)
-    eventCard.append(cardCol)
-    var eventTitle = document.createElement("h2")
+    var eventTitle = document.createElement("h2") 
     eventTitle.textContent = [data._embedded.events[i].name]
     eventCard.append(eventTitle);
     // card image
     var cardImage = document.createElement("img")
     var cardImageUrl = data._embedded.events[i].images[0].url
     cardImage.setAttribute('src', cardImageUrl)
-    cardImage.setAttribute('class', 'align-items-center')
+    cardImage.setAttribute('class', 'justify-content-center')
     cardImage.setAttribute('height', 250)
     cardImage.setAttribute('width', 350)
     eventCard.append(cardImage);
-    // venue name
-    var venueName = document.createElement("a")
-    venueName.setAttribute('class', 'text-center')
-    var venueURL = data._embedded.events[i]._embedded.venues[0].url?data._embedded.events[i]._embedded.venues[0].url:""
-    venueName.setAttribute('href', `${venueURL}`)
-    venueName.textContent = data._embedded.events[i]._embedded.venues[0].name
-    eventCard.append(venueName);
     // date and time
     var showDate = document.createElement("h3")
     showDate.setAttribute('class', 'text-center')
     var showTime = moment(data._embedded.events[i].dates.start.localTime, "H").format("LT")
     showDate.textContent = moment(data._embedded.events[i].dates.start.localDate, "YYYY-MM-DD").format("[The show is on ]dddd [the] Do [of] MMMM[ at ]") + showTime
     eventCard.append(showDate);
-    // tickets purchase and link
-    var ticketsURL = document.createElement("a")
-    ticketsURL.setAttribute('class', 'text-center')
-    ticketsURL.textContent = data._embedded.events[i].url
-    ticketsURL.setAttribute('class', 'text-center')
-    // var tickets = ("For ticket links, click here!")
-    // tickets.setAttribute('href', ticketsURL)
-    // console.log(tickets);
-    // eventCard.append(tickets);
-
+    // venue name & ticket link with artist name plain
+    var venueName = document.createElement("a")
+    var artistName = data._embedded.events[i]._embedded.attractions[0].name
+    venueName.setAttribute('class', 'text-center')
+    var ticketURL = data._embedded.events[i].url?data._embedded.events[i].url:""
+    venueName.setAttribute('href', `${ticketURL}`)
+    venueName.setAttribute('target', '_blank')
+    venueName.textContent = data._embedded.events[i]._embedded.venues[0].name + " - get your tickets for " + artistName + " here!"
+    eventCard.append(venueName);
+    
 }
 
 searchBtn.addEventListener("click", getAPI);

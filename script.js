6b00bf1cd94489ef177894fad4827ccd65a06bec
Nick Mason <nickmason372@yahoo.com>
2022-08-30 21:21:40 -0400

    updated files

--- script.js
+++ script.js
@@ -3,30 +3,36 @@ var citySearchEl = document.querySelector('#input-text');
 var searchBtn = document.querySelector('#searchBtn')
 
 // Datepicker widget
-$( function() {
-  $( "#datepicker" ).datepicker();
-  $( "#anim" ).on( "change", function() {
-    $( "#datepicker" ).datepicker( "option", "showAnim", $( this ).val() );
-  });
-} );
+// $( function() {
+//   $( "#datepicker" ).datepicker();
+//   $( "#anim" ).on( "change", function() {
+//     $( "#datepicker" ).datepicker( "option", "showAnim", $( this ).val() );
+//   });
+// } );
 
 // fake
 
-function handleSearchFormSubmit(event) {
+var handleSearchFormSubmit = function (event) {
   event.preventDefault();
+  // console.log(document.getElementById("input-text").value)
 
-  var searchInputVal = document.querySelector('#location-input').value;
+  var searchInputVal = document.getElementById('input-text').value
+  
+  
+
+  console.log(searchInputVal)
   // var formatInputVal = document.querySelector('#format-input').value;
   // var venueInputVal = document.querySelector('#venue-input').value;
 
   if (!searchInputVal) {
-    console.error('You need a search input value!');
+    console.log('You need a search input value!');
     return;
   }
-
-  var queryString = './results.html?q=' + searchInputVal + '&date=' + dateInputEl
+  localStorage.setItem('input', JSON.stringify(searchInputVal));
+  var queryString = './results.html?q=' + searchInputVal
 
   location.assign(queryString);
+  
 }
 
-searchBtn.addEventListener('submit', handleSearchFormSubmit);
+searchBtn.addEventListener('click', handleSearchFormSubmit);
